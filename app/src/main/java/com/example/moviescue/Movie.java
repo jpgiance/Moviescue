31f5b812050a3cafd55824ff5931358cf02d5a4f
jorgepg <jpgiance@gmail.com>
2019-12-31 14:34:54 -0600

    -Created Movie Class

---
+++ app/src/main/java/com/example/moviescue/Movie.java
@@ -0,0 +1,18 @@
+package com.example.moviescue;
+
+public class Movie {
+
+    private String title;
+    private String releaseDate;
+    private String overview;
+    private String imageLink;
+
+    private float voteAvg;
+    private float likeAvg;
+
+    public Movie(String movieTitle){
+        title = movieTitle;
+    }
+
+
+}
